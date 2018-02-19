0c37a7c2dc6472a25626d26bc77709a4491c422b
Lukas0610 <mail@lukasberger.at>
2018-02-19 16:45:52 +0100

    exynos7870-common: Add shim for OMX
    This fixes the damaged hardware-accelerated OMX-decoders, but not the underlying bug (invalid buffer-handles being passed around)
    
    For now, this is the best solution I can imagine.
    
    Change-Id: I459472110b1e8eff826d15bf77b199ff43f2e213

--- product/media.mk
+++ product/media.mk
@@ -30,4 +30,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libstagefright_shim \
-    libui_shim
+    libui_shim \
+    libExynosOMX_shim
+
+# Overriden service definition
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc
