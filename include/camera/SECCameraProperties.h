9794b7505e11550a332a4919c0ae9815b838f335
DarkLord1731 <darklord1731@gmail.com>
2018-02-22 13:01:18 +0530

    exynos7870-common: Remove PDAF and OIS from camera wrapper

--- include/camera/SECCameraProperties.h
+++ include/camera/SECCameraProperties.h
@@ -12,17 +12,3 @@
 #define METERING_MODE_SPORT 1
 #define METERING_MODE_MATRIX 2
 #define METERING_MODE_MANUAL 3
-
-/* Phase-Detection Auto Focus */
-#define AVAILABLE_PAF_MODE 0x80000005
-#define PAF_MODE 0x80000006
-/* Options */
-#define PAF_MODE_OFF 0
-#define PAF_MODE_ON 1
-
-/* Optical Image Stabilization Operation Mode */
-#define AVAILABLE_OIS_OPERATION_MODE 0x80020000
-#define OIS_OPERATION_MODE 0x80010000
-/* Options */
-#define OIS_OPERATION_MODE_PICTURE 0
-#define OIS_OPERATION_MODE_VIDEO 1
