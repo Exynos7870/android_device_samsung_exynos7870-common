838a01b750fe8f87de3b10a4f15ddee9fa0b0964
deadman96385 <seanhoyt963@gmail.com>
2018-02-26 03:54:13 -0600

    Add extract scripts

---
+++ setup-makefiles.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=samsung
+DEVICE_COMMON=exynos7870-common
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+
+# Copyright headers and guards
+write_headers "a3y17lte gtactive2lte gtaxlte j5y17lte j7velte j7xelte j7y17lte On7xelte"
+
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+
+OUTDIR=vendor/$VENDOR/$DEVICE_COMMON
+
+(cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libGLES_mali
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/vendor/lib/egl/libGLES_mali.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
+SYMLINKS := \$(TARGET_OUT)/vendor
+\$(SYMLINKS):
+	@echo "Symlink: vulkan.exynos5.so"
+	@mkdir -p \$@/lib/hw
+	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib/hw/vulkan.exynos5.so
+	@echo "Symlink: libOpenCL.so.1.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1.1
+ALL_MODULES.\$(LOCAL_MODULE).INSTALLED := \\
+	\$(ALL_MODULES.\$(LOCAL_MODULE).INSTALLED) \$(SYMLINKS)
+include \$(BUILD_PREBUILT)
+EOF
+
+(cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/$DEVICE_COMMON-vendor.mk
+
+# Create Mali links for Vulkan and OpenCL
+PRODUCT_PACKAGES += libGLES_mali
+EOF
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
+
+# Done
+write_footers
