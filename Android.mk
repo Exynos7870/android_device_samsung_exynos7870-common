556f4ffdad89199a1c64eb704ccefc9c641f63e0
DarkLord1731 <darklord1731@gmail.com>
2018-02-27 11:05:08 +0530

    exynos7870-common: Allow some more exynos7870 devices to use common

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter on7xelte j7xelte j7velte j7y17lte a3y17lte, $(TARGET_DEVICE)),)
+ifneq ($(filter a3y17lte gtactive2lte gtaxlte j5y17lte j7velte j7xelte j7y17lte on7xelte, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
