e4d339024749fa683986f29886df12639fbc8708
DarkLord1731 <siddhantnaik17@gmail.com>
2018-10-01 22:47:24 +0530

    exynos7870-common: Define correct CPU variant as cortex-a53

--- board/board.mk
+++ board/board.mk
@@ -29,7 +29,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_CORTEX_A53 := true
 
 # Binder
