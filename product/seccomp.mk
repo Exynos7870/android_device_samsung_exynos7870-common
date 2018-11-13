2667a56bcfad6e5858688c0ae1f79a3b1b4b4a60
DarkLord1731 <siddhantnaik17@gmail.com>
2018-11-13 21:17:22 +0530

    exynos7870-common: Update seccomp to oreo

--- product/seccomp.mk
+++ product/seccomp.mk
@@ -15,5 +15,5 @@
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/configs/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
